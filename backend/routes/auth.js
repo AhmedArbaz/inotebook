c6f3cd4a37be8b388937e626eefc3de8a6c43ba5
AhmedArbaz <ah2797764@gmail.com>
2024-07-13 14:36:09 +0500

    making authtoken in which make JWT str, and data which we select user_id that used for when it again login for verification id and secret str used for verifcation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const User = require("../models/User")
 const { body , validationResult } = require('express-validator');
+const bcrypt = require ('bcrypt');
+var jwt = require('jsonwebtoken')
+
+//ya jwt token bana rahay hain aur ya idealy config file ya .env may 
+const JWT_SECRET = 'Ahmedisagoodb$oy'; // ya secure jaga pay honi chiay eg env may ya string jwt banaty hoay sath may di jay gi nichay dakh lo jaha jwt banaya hay 
+
 
 //Create a User using: POST "/api/auth/createuser", No login required
 router.post('/createuser',[
@@ -27,14 +33,36 @@ try {
         return res.status(400).json({error:"Sorry a user with this email already exists"})
     }
 
+    //Creating password secure using bcrypt package we use in it salt
+    
+    // as well eg (bcrypt to hash bany ga lakin ager ksis nay bhot common password laga dia to ham admin honay ki vaja say khud say salt add kar rahay hain ya manually kuch aur add kar day ga hash may 
+
+    const salt = await bcrypt.genSalt(10); //ya bhi promise return karta hay ais liay await kia 
+    const secPass = await bcrypt.hash(req.body.password,salt);
+
+    //Finally user creating
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
         })
-    
-        //ager koi responce nahi bhajo gay to error ay ga ya yad rakhna 
-        res.json(user)
+        
+        //making JWT Token ais may 2 things hoti hay data, secret to ham nay data may id di, secret may ham khud say ak string banaty hain vo day di 
+        const data = {
+            user:{
+                id:user.id //ya ham id bhaj rahay hain user ko data may pura user ki info nahi 
+            }
+        }
+
+        const authToken = jwt.sign(data,JWT_SECRET)
+
+        //ager koi responce nahi bhajo gay to error ay ga ya yad rakhna ya user nahi dia jata token dia jata hay vo abhi banaya hay vo dain gay
+        // res.json(user)
+
+        res.json({authToken:authToken})
+
+
+
 } catch (error) {
     console.log(error.message);
     res.status(500).send("Some error occured")
