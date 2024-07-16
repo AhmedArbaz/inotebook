33a042b419fba584adb3860f35c8fde1483a6eb6
AhmedArbaz <ah2797764@gmail.com>
2024-07-16 14:02:59 +0500

    Removing useContext hook because its only for understanding now doing proper work

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,15 +1,10 @@
-import { useContext ,useEffect} from "react" //ya use karna tha to useContext use kia 
-import noteContext from "../Context/notes/noteContext" //note Context say banaya hay to aus ko import kia aur dakho nichy use kar lia 
+
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update();
-       //eslint-disable-next-line
-    }, );
+ 
   return (
     <div>
-      This is about {a.state.name} and he is in class {a.state.class}
+      This is about 
     </div>
   )
 }
--- src/Context/notes/NotesState.js
+++ src/Context/notes/NotesState.js
@@ -1,28 +1,16 @@
 import noteContext from "./noteContext";
 //ya import kia bany hoay context ko aur use kia 
-import { useState } from "react";
+
 
 const NoteState = (props)=>{
 
     // This is an object we make it to show how we can make obj in useContext
-    const s1 = {
-    "name":"Ahmed",
-    "class":"10"
-}
+ 
 
-// Making State state 
-const [state, setState] = useState(s1);
-const update = ()=>{
-setTimeout(() => {
-    setState({
-       "name":"Ahmed Arbaz",
-        "class":"12" 
-    })
-}, 1000);
-}
+
     return (
         // noteContext file may ais context ko banaya hay aur yaha ausi context ko use kia hay 
-        <noteContext.Provider value={{state:state,update:update}}>
+        <noteContext.Provider value={{}}>
             {props.children} 
          {/* ais ka porps.childern ka matlab jitnay bhi component hain aun ko as props pass ho jay */}
         </noteContext.Provider>
